c2fb319ff7cbbc8851ec8d9f804da97d0293da23
AshecOne <haseyong123@gmail.com>
2024-12-11 03:18:48 +0800

    fix create product

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,11 @@ import app from "./config/app";
 import sequelize from "./config/database";
 import authRoutes from "./routes/authRoutes";
 import productRoutes from "./routes/productRoutes";
+import express from "express";
+import path from "path";
 
 const PORT = process.env.PORT || 3001;
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
